38a60ef41060852303c999abba641e7f52ba44c6
jpertuz <jpertuzegea@hotmail.com>
2020-12-22 22:46:49 -0500

    TASK_#001 - Submit ok

--- CRUDAngularFront/src/app/Modules/Administracion/usuarios/usuarios.component.ts
+++ CRUDAngularFront/src/app/Modules/Administracion/usuarios/usuarios.component.ts
@@ -1,3 +1,5 @@
+
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -5,12 +7,25 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './usuarios.component.html',
   styleUrls: ['./usuarios.component.css']
 })
+
 export class UsuariosComponent implements OnInit {
   showModal = false;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
+  form: FormGroup;
+
 
   ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      Nombre: ''
+    });
+     
+  }
+
+  submit() {
+
+    alert("Mi Nombre es: " + this.form.get("Nombre").value);
+
   }
 
 }
