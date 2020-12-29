9212fb83f19f126cfa349684beb9dc5362cd7c74
jpertuz <jpertuzegea@hotmail.com>
2020-12-29 09:07:48 -0500

    Ajuste importacion FormsModule

--- CRUDAngularFront/src/app/app.module.ts
+++ CRUDAngularFront/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms'; 
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component'; 
@@ -19,7 +19,9 @@ import { AgregarComponent } from './Modules/Administracion/usuarios/agregar/agre
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule, ReactiveFormsModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
